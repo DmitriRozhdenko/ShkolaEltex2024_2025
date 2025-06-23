0fb51be96a846c736661aa1a37391961830f7969
dim <rozhdenkoDm@yandex.ru>
2025-06-24 02:13:26 +0700

    в 4 задании измён порядок очистка буфера scanf.Добавлено 1 задача 5ого задания

--- Zadanie5/zadacha1/AbntSpravchFunc.c
+++ Zadanie5/zadacha1/AbntSpravchFunc.c
@@ -5,33 +5,33 @@
 			char second_name[10];
 			char tel[10];
 	  };
+	  
+  void MenySpravochnik(unsigned char *vcRabotaFn); 													/// МЕНЮ СПРАВОЧНИКА
+  void InputKomandMeny(unsigned char *vcRabotaFn); 													/// ВВОД КОМАНДЫ МЕНЮ
+  void AddAbonent(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,	struct abonent *ptrFn);  /// Выполнение команды
+																										 /// " Добавить абонента "
+  void DellAbonent(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,	struct abonent *ptrFn);  /// УДАЛЕНИЕ АБОНЕНТОВ 
+  void OutPutAllRecord(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,struct abonent *ptrFn); /// ВЫВОД ВСЕХ ЗАПИСЕЙ
+ void SearchAbonent (unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,struct abonent *ptrFn	); /// ПОИСК АБОНЕНТОВ 
 
  int main() 
  {  
 	unsigned char i,k;
-	unsigned  int vcKomanda ;	// номер команды, вводимой с клавиатуры
 	unsigned char vcCount_Abonent; // счетчик количества абонентов в списке
-	unsigned char vcDel_Abonent;   //номер абонента,которого нужно удалить
-	unsigned char vcIstina;			// результат сравнения
-	unsigned char  vcOperaciya;		// выполняемая операция при добавлении абонента
 	unsigned char vcRabota ;		// текущий номер выполняемого блока в switch
-	unsigned char Temp[10],Temp1[10]; 
-	unsigned char vcCount_Sovpadenij;//счетчик совпадений имен при поиске абонентов
-
 		  
-	 struct abonent Caller[100]; 
-	 struct abonent	*ptr;
-	 struct abonent	*ptrTEMP;
+	 struct abonent Caller[100]; 	// обьявили 100 абонентов
+	 struct abonent *ptrtemp;			
 	 
 	 //очистка полей справочника
 	  for(i=0;i<100;i++)
 	   {  
-		   ptr=&Caller[i];
+		   ptrtemp=&Caller[i];
 		  for(k=0;k<10;k++) 
 		   {
-			   ptr->name[k]=0;
-			   ptr->second_name[k]=0;
-			   ptr->tel[k]=0;
+			   ptrtemp->name[k]=0;
+			   ptrtemp->second_name[k]=0;
+			   ptrtemp->tel[k]=0;
 		   }
 	   }
 	   vcRabota=0;
@@ -42,6 +42,39 @@
 		switch  (vcRabota)
         {
 		   case 0:    ///  МЕНЮ СПРАВОЧНИКА
+				MenySpravochnik( &vcRabota );
+		   break;
+		   case 1:     /// ВВОД КОМАНДЫ МЕНЮ
+		     InputKomandMeny( &vcRabota );
+		   break;
+		   case 2:     /// Выполнение команды " Добавить абонента "
+				AddAbonent(&vcCount_Abonent, &vcRabota, &Caller[vcCount_Abonent]);
+		   break;
+		   case 3:     /// УДАЛЕНИЕ АБОНЕНТОВ 
+	              
+	             DellAbonent(&vcCount_Abonent, &vcRabota,&Caller[0]); 	   
+		    break;
+		    case 4: /// ПОИСК АБОНЕНТОВ 
+		          SearchAbonent (&vcCount_Abonent, &vcRabota, &Caller[0]);
+		    break;	    
+		    case 5:     /// ВЫВОД ВСЕХ ЗАПИСЕЙ
+					OutPutAllRecord(&vcCount_Abonent,&vcRabota,&Caller[0]) ;
+		    break;
+		    case 6:		/// Выход из меню справочника
+		         exit(1);
+				vcRabota=0;	
+		    break;
+		  } 
+	}	
+	
+}	
+
+
+
+
+
+void MenySpravochnik(unsigned char *vcRabotaFn)
+ {
 				printf("__________________________ \n");   
 				printf("	 МЕНЮ справочника \n");
 				printf("			 \n");
@@ -53,33 +86,41 @@
 				printf("__________________________ \n"); 
 				printf("			 \n");
 				printf("введите пункт меню: ");
-			 
-				
-				vcRabota=1;
-			  // vcCountCyfr=0;
-		   break;
-		    case 1:     /// ВВОД КОМАНДЫ МЕНЮ
-		   
-				scanf("%d",&vcKomanda);
-				scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков
-				  if((vcKomanda == 0)|| (vcKomanda >5)) //проверка в диапазоне ли вводимая команда меню
-				  {
-					printf("Нет такого пункта меню \n");
+			 			
+				*vcRabotaFn=1; 
+ }	 
+ 
+ void InputKomandMeny(unsigned char *vcRabotaFn)
+ {
+	unsigned  int vcKomanda ;	// номер команды, вводимой с клавиатуры
+    
+	scanf("%d",&vcKomanda);
+	scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков
+	  if((vcKomanda == 0)|| (vcKomanda >5)) //проверка в диапазоне ли вводимая команда меню
+	  {
+		printf("Нет такого пункта меню \n");
+
+	  }
+	  else 
+	  {	
+		*vcRabotaFn= vcKomanda + 1; 		  				
+	  } 
+ }	 
+
+void AddAbonent(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,	struct abonent *ptrFn	)
+{
+		unsigned char i,k;
+		unsigned char Temp[10] ;
+		 unsigned char  vcOperaciya ;		// выполняемая операция при добавлении абонента
 		
-				  }
-				  else 
-				  {	
-					vcRabota= vcKomanda + 1; 	
-					vcOperaciya=0;	  				
-				  }
-	   
-		   break;
-		   case 2:     /// Выполнение команды " Добавить абонента "
-				printf("			 \n");
+		vcOperaciya = 0;
+	    while( vcOperaciya < 3 )
+	    {
+			printf("			 \n");
 				 switch (vcOperaciya)
 				     { 
 					   case 0:
-						   if(vcCount_Abonent < 100) 
+						   if(*vcCount_AbonentFn < 100) 
 							{ printf("введите имя абонента (не больше 10знаков) \n");	
 								 for(k=0;k<10;k++) 
 								   {
@@ -87,17 +128,17 @@
 								   }						
 							  scanf("%10s",Temp);
 							  scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков	
-							  ptr=&Caller[vcCount_Abonent];
+							 // ptr=&Caller[vcCount_Abonent];
 							  for(i=0;i<10;i++)
 								{ 
-									ptr->name[i] = Temp[i];			      
+									ptrFn->name[i] = Temp[i];			      
 								}
 							 vcOperaciya =1;   
 							}
 							 else 
 							 { 
 								 printf("превышено количество добавляемых абонентов  \n");
-								 vcRabota=0;
+								 *vcRabotaFn=0;
 							 }
 					      
 					   break;						   				   
@@ -112,7 +153,7 @@
 							  scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков	
 							  for(i=0;i<10;i++)
 								{ 
-									ptr->second_name[i] = Temp[i];			      
+									ptrFn->second_name[i] = Temp[i];			      
 								}
 							 vcOperaciya =2;       
 					   break;					   
@@ -121,41 +162,59 @@
 								for(k=0;k<10;k++) 
 								   {
 									  Temp[k] =0; 
-								   }									 						
+								   }	
+								 						
 							  scanf("%10s",Temp);
-							  scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков
+							  scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков	
 							  for(i=0;i<10;i++)
 								{ 
-									ptr->tel[i] = Temp[i];			      
+									ptrFn->tel[i] = Temp[i];			      
 								}
-									vcOperaciya = 0; 
-										vcRabota = 0;
-								 vcCount_Abonent = vcCount_Abonent +1;
-					   break;				   					 
-					 }	   
-		   break;
-		   case 3:     /// УДАЛЕНИЕ АБОНЕНТОВ 
-	              if( vcCount_Abonent > 0)
+									vcOperaciya = 3; 
+									*vcRabotaFn = 0;	
+								 *vcCount_AbonentFn = *vcCount_AbonentFn +1;
+					   break;	
+					   	   					 
+					 }	
+					 
+			}	
+				 	    
+}	
+	
+/// УДАЛЕНИЕ АБОНЕНТОВ 
+void DellAbonent(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,  //			
+		struct abonent *ptrFn	)
+ {
+	unsigned char i,k;
+	unsigned char Temp[10],Temp1[10];
+	unsigned char vcDel_Abonent;   //номер абонента,которого нужно удалить	 
+	struct abonent	*ptr;
+	 struct abonent	*ptrTEMP;
+	 unsigned char vcIstina;			// результат сравнения
+
+	              if( *vcCount_AbonentFn > 0) 
 	               {
 					    printf("ВВЕДИТЕ ИМЯ АБОНЕНТА,КОТОРОГО НУЖНО УДАЛИТЬ\n");
 						 for(k=0;k<10;k++) 
 						   {
 							  Temp[k] =0; 
-						   } 						   
+						   } 
+						   
 						   scanf("%10s",Temp);
 					   		scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков			   
 				      printf("ВВЕДИТЕ ФАМИЛИЮ АБОНЕНТА,КОТОРОГО НУЖНО УДАЛИТЬ\n");
 						 for(k=0;k<10;k++) 
 						   {
 							  Temp1[k] =0; 
-						   } 						   
+						   } 
+						   
 						   scanf("%10s",Temp1);
 							scanf("%*[^\n]");   // очистка буфера scanf,если ввели больше 10 знаков	
 							vcDel_Abonent = 0;
 							// ищем абонента ,которого нужно удалить
-					     for(i=0;i < vcCount_Abonent;i++)
+					     for(i=0;i < *vcCount_AbonentFn;i++)
 					      {
-							 ptr=&Caller[i];
+							ptr = &ptrFn[i];
 							 vcIstina =0;
 							  for(k=0;k<10;k++)
 							  {	
@@ -180,7 +239,7 @@
 						  
 						  if(vcDel_Abonent > 0)
 						   {
-							   ptr=&Caller[vcDel_Abonent-1];
+							   ptr=&ptrFn[vcDel_Abonent-1];
 							   // обнуление полей абонента
 							  for(k=0;k<10;k++) 
 							   {
@@ -189,10 +248,10 @@
 								   ptr->tel[k]=0;
 							   }
 								// сдвинуть абонентов,чтобы не было пустот в списке абонентов при выводе их на экран
-							    for(i=(vcDel_Abonent-1);i < vcCount_Abonent; i++)
+							    for(i=(vcDel_Abonent-1);i < *vcCount_AbonentFn; i++)
 								   {  
-									   ptr=&Caller[i];
-									   ptrTEMP=&Caller[i+1];
+									   ptr=&ptrFn[i];
+									   ptrTEMP=&ptrFn[i+1];
 									  for(k=0;k<10;k++) 
 									   {
 										   ptr->name[k]			= ptrTEMP->name[k];
@@ -200,7 +259,7 @@
 										   ptr->tel[k]			= ptrTEMP->tel[k];
 									   }
 								   }
-							   vcCount_Abonent =vcCount_Abonent -1;
+							   *vcCount_AbonentFn =*vcCount_AbonentFn -1;
 						   } 
 						   else
 						   {
@@ -213,10 +272,62 @@
 						 printf("НЕТ АБОНЕНТОВ В СПИСКЕ\n");
 						 printf("			 \n"); 	
 					    }		   
-						  	vcRabota=0;		   
-		    break;
-		    case 4:     /// ПОИСК АБОНЕНТОВ 
-	              if( vcCount_Abonent > 0)
+						  	*vcRabotaFn=0;	
+}
+
+/// ВЫВОД ВСЕХ ЗАПИСЕЙ
+void OutPutAllRecord(unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,struct abonent *ptrFn	)
+{
+	unsigned char i,k;
+	struct abonent	*ptr;
+	
+	 
+					if( *vcCount_AbonentFn > 0)
+					 {
+						 printf("список абонентов\n");
+						  printf("			 \n"); 
+					     for(i=0; i< *vcCount_AbonentFn;i++)
+					      {
+							 ptr=&ptrFn[i];
+							 printf("%d.  ",i+1);
+							 for(k=0;k<10;k++)
+							  {	
+								  printf("%c",ptr->name[k]);								 
+							  }  
+							   printf("			 \n"); 
+							   printf("  "); 
+							  for(k=0;k<10;k++)
+							  {	
+								  printf("%c",ptr->second_name[k]);								 
+							  }  
+							   printf("			 \n"); 
+							   printf("  "); 
+							  for(k=0;k<10;k++)
+							  {	
+								  printf("%c",ptr->tel[k]);								 
+							  }  
+							   printf("			 \n"); 	
+							   	printf("			 \n");					 
+						  }	
+						}
+						else 
+						 { printf("СПИСОК АБОНЕНТОВ ПУСТ\n");
+						   printf("			 \n"); 
+						 }  
+						  	*vcRabotaFn=0;	
+}						  		
+
+
+/// ПОИСК АБОНЕНТОВ 
+void SearchAbonent (unsigned char *vcCount_AbonentFn,unsigned char *vcRabotaFn,struct abonent *ptrFn	)
+{
+		unsigned char vcCount_Sovpadenij;	//счетчик совпадений имен при поиске абонентов
+		unsigned char i,k;
+		struct abonent	*ptr;
+		unsigned char Temp[10];
+		unsigned char vcIstina;			// результат сравнения
+		
+	             if( *vcCount_AbonentFn > 0)
 	               {
 				      printf("ВВЕДИТЕ ИМЯ АБОНЕНТА,КОТОРОГО НУЖНО НАЙТИ\n");
 						 for(k=0;k<10;k++) 
@@ -228,9 +339,9 @@
 						   scanf("%*[^\n]");  // очистка буфера scanf,если ввели больше 10 знаков	
 						   scanf("%10s",Temp);
 						
-					     for(i=0;i < vcCount_Abonent;i++)
+					     for(i=0;i < *vcCount_AbonentFn;i++)
 					      {
-							 ptr=&Caller[i];
+							 ptr=&ptrFn[i];
 							 vcIstina =0;
 							  for(k=0;k<10;k++)
 							  {	
@@ -284,52 +395,5 @@
 					 printf("СПРАВОЧНИК НЕ ЗАПОЛНЕН \n");
 					 printf("			 \n"); 	
 					}		   
-						  	vcRabota=0;		   
-		    break;
-	
-		    
-		    case 5:     /// ВЫВОД ВСЕХ ЗАПИСЕЙ
-					if( vcCount_Abonent > 0)
-					 {
-                          printf("			 \n");
-						 printf("список абонентов\n");
-						  printf("			 \n"); 
-					     for(i=0;i < vcCount_Abonent;i++)
-					      {
-							 ptr=&Caller[i];
-							 printf("%d.  ",i+1);
-							 for(k=0;k<10;k++)
-							  {	
-								  printf("%c",ptr->name[k]);								 
-							  }  
-							   printf("			 \n"); 
-							   printf("  "); 
-							  for(k=0;k<10;k++)
-							  {	
-								  printf("%c",ptr->second_name[k]);								 
-							  }  
-							   printf("			 \n"); 
-							   printf("  "); 
-							  for(k=0;k<10;k++)
-							  {	
-								  printf("%c",ptr->tel[k]);								 
-							  }  
-							   printf("			 \n"); 	
-							   	printf("			 \n");					 
-						  }	
-						}
-						else 
-						 { printf("СПИСОК АБОНЕНТОВ ПУСТ\n");
-						   printf("			 \n"); 
-						 }  
-						  	vcRabota=0;		   
-		    break;
-		    case 6:		// Выход из меню справочника
-		         exit(1);
-				vcRabota=0;	
-		    break;
-		  } 
-	}	
-	
-}	
-
+						  	*vcRabotaFn=0;		
+}
